2beca89f4c9498ac83f458558904e1bb9025ce1e
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-07 23:05:49 +0530

    creats the BookList component renders same as before and passes all the tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 
 import React from 'react'
 import './App.css';
-
-const renderBooks= (books)=>{
-    return (<div className="books">
-      {
-        books.map(book => {
-          return (<div className="book">
-            <h2 className="title">{book.name}</h2>
-          </div>)
-        })
-      }
-    </div>)
-  }
+import BooksList from './BookList';
 
 function App() {
     const books =[{name:"React"},{name:'Redux'}]
   return (
     <div className="App">
       <h1>Library</h1>
-      {renderBooks(books)}
+     <BooksList books ={books}/>
     </div>
   );
 }
